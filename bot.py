fd127aae6a47597a26c1d91cc8187c25842ad7c9
Edgar Gonçalves <edgar.gomes.goncalves@zalando.de>
2021-11-30 19:01:19 +0100

    Bot running in discord - sends three sports covers.

--- bot.py
+++ bot.py
@@ -1,50 +1,26 @@
-from bs4 import BeautifulSoup, element
-import requests
+import discord
+import covers
 
 
-def _get_pictures() -> element.ResultSet:
-    # Grab html
-    r = requests.get('https://24.sapo.pt/jornais/desporto')
-
-    # Parse to something edible
-    soup = BeautifulSoup(r.content, features='html.parser')
-
-    # Find all elements tagged with picture
-    pictures = soup.findAll('picture')
-
-    return pictures
+client = discord.Client()
+token = open('token', 'r').read()
 
 
-def _filter_pictures(pictures, jornais) -> list:
-    # Return the covers we want
-    covers = [
-        cover['data-original-src'] for cover in pictures if cover.get('data-title', '').startswith(jornais)
-    ]
-    return covers
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
 
-def _iterate_covers(covers):
-    if len(covers) == 1:
-        print(covers[0])
-    else:
-        print(covers[0])
-        covers.pop(0)
-        _iterate_covers(covers)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
+    if message.content.startswith('$capas'):
+        # embed_content_in_dict = message.embeds[0].to_dict()
+        # await message.channel.send("Hello!")
+        capas = covers.sports_covers()
+        for capa in capas:
+            await message.channel.send(capa)
 
-def main():
-    """
-    This script should return links to the covers of the newspapers in jornais tuple
-    :return: https://ia.imgs.sapo.pt/...
-    """
-    jornais = ('A Bola', 'O Jogo', 'Record')
-
-    pictures = _get_pictures()
-
-    covers = _filter_pictures(pictures, jornais)
-
-    return _iterate_covers(covers)
-
-
-if __name__ == '__main__':
-    main()
+client.run(token)
---
+++ covers.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup, element
+import requests
+
+
+def _get_pictures() -> element.ResultSet:
+    # Grab html
+    r = requests.get('https://24.sapo.pt/jornais/desporto')
+
+    # Parse to something edible
+    soup = BeautifulSoup(r.content, features='html.parser')
+
+    # Find all elements tagged with picture
+    pictures = soup.findAll('picture')
+
+    return pictures
+
+
+def _filter_pictures(pictures, jornais) -> list:
+    # Return the covers we want
+    covers = [
+        cover['data-original-src'] for cover in pictures if cover.get('data-title', '').startswith(jornais)
+    ]
+    return covers
+
+
+def sports_covers():
+    """
+    This script should return links to the covers of the newspapers in jornais tuple
+    :return: https://ia.imgs.sapo.pt/...
+    """
+    jornais = ('A Bola', 'O Jogo', 'Record')
+
+    pictures = _get_pictures()
+
+    covers = _filter_pictures(pictures, jornais)
+
+    return covers
+
+
+if __name__ == '__main__':
+    sports_covers()
